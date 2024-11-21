de367d1c1f579ba7711a55f6f824f36bbeba03e3
ivan <1@1.com>
2024-11-21 14:42:04 +0300

    Reduce connections attempts, increase sleep time

--- core/utils/error_helper.py
+++ core/utils/error_helper.py
@@ -18,7 +18,7 @@ class FailureCounter:
 
         self.id = None
 
-        self.limit = 5
+        self.limit = 2
 
     def fail_increment(self, step: float = 1):
         self.fail_count += step
@@ -40,7 +40,7 @@ class FailureCounter:
             if is_raise:
                 raise_error(FailureLimitReachedException(self.fail_count))
             else:
-                sleep_time = random.randint(2, 5) * 60
+                sleep_time = random.randint(60, 120) * 60
                 msg = f"{self.id} | Sleeping for {int(sleep_time)} seconds... Too many errors. Retrying..."
                 await self.reset_with_delay(msg, sleep_time)
         else:
